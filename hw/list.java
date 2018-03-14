beea7c1dcf27fc5c914f58fd0718920863c96b15
takagotch <dyaccb@gmail.com>
2018-03-15 08:31:34 +0900

    button.setLayoutParams(new TableRow.LayoutParams(WC, WC));

---
+++ hw/list.java
@@ -0,0 +1,26 @@
+//onRequestPermissionsResult
+public void onRequestPermissionResult(int code, String[] perms, int[] results)
+
+@Override
+public void onRequestPermissionResult(int code, String[] perms, int[] results){
+  if(code == 1 && results[] == PackageManager.PERMISSION_GRANTED){
+  }
+}
+
+
+//requestPermission
+public static void requestPermissions(Activity activity,
+  String[] perms, int code)
+
+//checkSelfPermission
+public static int checkSelfPermission(Context context, String perm)
+
+//
+
+
+
+
+
+
+
+
