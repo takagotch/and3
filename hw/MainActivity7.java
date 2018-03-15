d202deeeecce68b47c1461686e43e4291cd613af
takagotch <dyaccb@gmail.com>
2018-03-16 00:41:43 +0900

    mp.setDataSource(Environment.getExtrnalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getPath() + /sample.mp3);

--- hw/MainActivity7.java
+++ hw/MainActivity7.java
@@ -17,18 +17,36 @@ public class MainAcitivity extends AppCompatActivity{
   private List<Sensor> list;
 
   @Override
-  protected void onCreate(){
-  
+  protected void onCreate(Bundle saveInstanceState){
+    super.onCreate(saveInstanceState);
+    setContentView(R.layout.activity_main);
+
+    manager = () getSystemService();
+    list = manager.getSensorList();
+
+    listener = new SensorEvnetListener(){
+      public void onSensorChanged(SensorEvent event){
+        Toast.makeText(MainActivity.this,
+		Taost.makeText(MainActivity.this,
+			"ACC" + event.values[0], Toast.LENGTH_LONG).show());
+      }
+      public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+    };
   }
   
   @Override
   protected void onResume(){
-  
+    super.onResume();
+    if(list.size() > 0){
+      manager.registerListener(listener, list.get(0)
+	SensorManager.SENSOR_DELAY_NORMAL);
+    }
   }
 
   @Override
   protected void onPause(){
-  
+    super.onPause();
+    manager.unregisterListener(listener, list.get(0));
   }
 }
 
