d9fd10db37230e148c1546da490cb152579aa092
takagotch <dyaccb@gmail.com>
2018-03-18 08:49:31 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- v7/SimpleSurface.java
+++ v7/SimpleSurface.java
@@ -15,5 +15,41 @@ public class SimpleSurface extends SurfaceView {
     super(context, attrs, defStyle);
     init();
   }
+
+  public SimpleSurface(Context context, AttributeSet attrs){
+    super(context, attrs);
+    init();
+  }
+
+  public SimpleSurface(Context context){
+    super(context);
+    init();
+  }
+
+  private void init(){
+    p = new Paint();
+    p.setColor(Color.CYAN);
+    getHolder().addCallback(
+	new SurfaceHolder.Callback(){
+	  public void surfaceChanged(SurfaceHolder holder,
+		int format, int width, int height){
+	  }
+
+	  public void surfaceCreate(SurfaceHolder holder){
+	    draw(holder);
+	  }
+
+	  public void surfaceDestroyed(SurfaceHolder holder){
+	  }
+	}
+    );
+  }
+
+  private void draw(SurfaceHolder holder){
+    Canvas canvas = holder.lockCanvas();
+    canvas.drawColor(Color.WHITE);
+    canvas.drawRect(100, 100, 400, 400, p);
+    holder.unlockCanvasAndPost(canvas);
+  }
 }
 
