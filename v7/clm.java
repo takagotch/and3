09bcfe11808052c5fec5ae1cfc6d23fa8662e2b3
takagotch <dyaccb@gmail.com>
2018-03-18 08:38:06 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- v7/clm.java
+++ v7/clm.java
@@ -3,3 +3,18 @@ canvas.translate(400, 400);
 canvas.drawRect(0, 0, 300, 300, p);
 canvas.restore();
 canvas.drawRect(0, 0, 300, 300, p);
+
+//CanvasView.java CanvasMultiTouch
+public boolean onTouchEvent(MotionEvent event){
+  for(int i = 0; i < event.getPointerCont(); i++){
+    Log.d("MultiTouch",
+	String.format("ID %s > [%s, %s]", event
+	  .getPinterId(i),
+	event.getX(i),
+	event.getY(i)
+	)
+	);
+  }
+  return true;
+}
+
