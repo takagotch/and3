062a5e53a7f9f46e5522e1c6b98f4ecd24319ff8
takagotch <dyaccb@gmail.com>
2018-03-19 01:48:38 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- v6/clm.java
+++ v6/clm.java
@@ -1,6 +1,14 @@
 //setTitle/setMessage
 builder.setTitle(R.string.d_title);
 
+switch(which){
+	case DialogInterface.BUTTON_POSITIVE:
+		break;
+	case DialogInterface.BUTTON_NEGATIVE:
+		break;
+	case DialogInterface.BUTTON_NEUTRAL:
+		break;
+}
 
 
 
