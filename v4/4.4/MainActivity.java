8beaa1c140754f5fd6c0bdc31fa102542d56ac9b
takagotch <dyaccb@gmail.com>
2018-03-20 04:18:17 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ v4/4.4/MainActivity.java
@@ -0,0 +1,36 @@
+package com.tky.listactivity;
+
+import android.app.ListActivity;
+import andorid.os.Bundle;
+import android.view.View;
+import andoird.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity{
+  ArrayAdapter<String> adapter;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+
+    final ArrayList<String> data = new ArrayList<>();
+    data.add("a");
+    data.add("b");
+    data.add("c");
+    data.add("e");
+    data.add("f");
+
+    adapter = new ArrayAdapter<>(
+	this, android.R.layout.simple_list_item_1, data);
+    this.setListAdapter(adapter);
+  }
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int positon, long id){
+    super.onListItemClick(l, v, position, id);
+    adapter.remove((String) ((TextView) v).getText());
+  }
+}
+
